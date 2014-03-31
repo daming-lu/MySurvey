4719b060e57986c765bce3f7d73b911bc9576b51
damingl <damingl@zoosk.com>
2014-03-30 17:37:50 -0700

    checkin

--- PHP/test1.php
+++ PHP/test1.php
@@ -1,5 +1,8 @@
 <?php
-$myusername=$_POST['username'];
-file_put_contents("../logs/log1","username : ".$myusername."\n", FILE_APPEND|LOCK_EX);
+$myusername=$_POST['username1'];
+file_put_contents("../logs/log1","username1 : ".$myusername."\n", FILE_APPEND|LOCK_EX);
 file_put_contents("../logs/log2","_POST : ".print_r($_POST,true)."\n", FILE_APPEND|LOCK_EX);
+	
+header("location: http://peirongli.dreamhosters.com/MySurvey/MainPage.html");
+
 ?>
