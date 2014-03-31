398b6eadeb1027a8ccaad5dd4089e0d468ceb474
damingl <damingl@zoosk.com>
2014-03-30 17:32:47 -0700

    this time test _POST

--- PHP/test1.php
+++ PHP/test1.php
@@ -1,4 +1,5 @@
 <?php
 $myusername=$_POST['username'];
 file_put_contents("../logs/log1","username : ".$myusername."\n", FILE_APPEND|LOCK_EX);
+file_put_contents("../logs/log2","_POST : ".print_r($_POST,true)."\n", FILE_APPEND|LOCK_EX);
 ?>
